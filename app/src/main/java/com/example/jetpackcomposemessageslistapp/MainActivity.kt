92a9e1d55b0ae6721a36500ffcb8b3a2015692a7
TeniG <tenigada@gmail.com>
2023-09-29 20:47:25 +0530

    add and display message in list implemented

---
+++ app/src/main/java/com/example/jetpackcomposemessageslistapp/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.jetpackcomposemessageslistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposemessageslistapp.ui.theme.ComposeMessagesListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeMessagesListAppTheme {
+                var message by remember {
+                    mutableStateOf("")
+                }
+                var messageList by remember {
+                    mutableStateOf(listOf<String>())
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(16.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        OutlinedTextField(
+                            value = message,
+                            onValueChange = { text ->
+                                message = text
+                            },
+                            placeholder = {
+                                Text(text = "Enter Message")
+                            },
+                            modifier = Modifier.weight(1f)
+
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(onClick = {
+                            messageList = messageList + message
+                            message = ""
+                        }) {
+                            Text(text = "Add")
+                        }
+                    }
+                    LazyColumn() {
+                        items(messageList) { item ->
+                            Text(
+                                text = item,
+                                modifier = Modifier.padding(16.dp)
+                            )
+                            Divider()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
